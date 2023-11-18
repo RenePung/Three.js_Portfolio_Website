887aa55931c0f148aae4b2e693c82a9b96e5f3ea
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-18 18:19:05 +0100

    created simple navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,14 @@ const Navbar = () => {
         <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md border border-sky-300">
             <p className="blue-gradient_text border border-sky-200 rounded-lg">RP</p>
         </NavLink>
-        <nav></nav>
+        <nav className="flex text-lg gap-7 font-medium">
+            <NavLink to="/about" className={({ isActive }) => isActive ? 'text-sky-400' : 'text-black'}>
+                About
+            </NavLink>
+            <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-sky-400' : 'text-black'}>
+                Projects
+            </NavLink>
+        </nav>
     </header>
   )
 }
