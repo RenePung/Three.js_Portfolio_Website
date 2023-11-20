84a3e600d4c66e1b09bf7c29cbae70c89051df49
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-20 12:02:31 +0100

    fixed bug - unable to rotate with the mouse. added custom use frame hook

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -20,7 +20,7 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
   const { gl, viewport } = useThree();
   const { nodes, materials } = useGLTF(islandScene);
 
-  const lastx = useRef(0);
+  const lastX = useRef(0);
   const rotationSpeed = useRef(0);
   const dampingFactor = 0.95;
 
@@ -40,8 +40,14 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
   e.stopPropagation(); // Prevents the event from propagating up the DOM hierarchy
   e.preventDefault(); // Prevents the default action associated with the event
   setIsRotating(false); // Sets the state variable isRotating to false
+  }
 
-  const clientX = e.touches 
+  const handlePointerMove = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    if(isRotating) {
+      const clientX = e.touches 
     ? e.touches[0].clientX // Retrieves the X-coordinate of the pointer, accounting for touch events
     : e.clientX;
 
@@ -50,13 +56,7 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
   islandRef.current.rotation.y += delta * 0.01 * Math.PI; // Updates the rotation of the island based on the calculated delta
   lastX.current = clientX; // Updates the lastX.current to the current X-coordinate for the next event
   rotationSpeed.current = delta * 0.01 * Math.PI; // Updates the rotationSpeed.current based on the calculated delta
-  }
-
-  const handlePointerMove = (e) => {
-    e.stopPropagation();
-    e.preventDefault();
-
-    if(isRotating) handlePointerUp(e);
+    }
   }
 
   const handleKeyDown = (e) => {
@@ -75,17 +75,50 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
     }
   }
 
+  useFrame(() => {
+    if(!isRotating) {
+      rotationSpeed.current *= dampingFactor;
+
+      if(Math.abs(rotationSpeed.current) < 0.001) {
+        rotationSpeed.current = 0;
+      }
+    } else {
+      const rotation = islandRef.current.rotation.y;
+
+      const normalizedRotation = ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      // Set the current stage based on the island's orientation
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          setCurrentStage(1);
+          break;
+        default:
+          setCurrentStage(null);
+      }
+    }
+  });
+
   useEffect(() => {
-    document.addEventListener('pointerdown', handlePointerDown);
-    document.addEventListener('pointerup', handlePointerUp);
-    document.addEventListener('pointermove', handlePointerMove);
+    const canvas = gl.domElement;
+    canvas.addEventListener('pointerdown', handlePointerDown);
+    canvas.addEventListener('pointerup', handlePointerUp);
+    canvas.addEventListener('pointermove', handlePointerMove);
     document.addEventListener('keydown', handleKeyDown);
     document.addEventListener('keyup', handleKeyUp);
 
     return () => {
-      document.removeEventListener('pointerdown', handlePointerDown);
-      document.removeEventListener('pointerup', handlePointerUp);
-      document.removeEventListener('pointermove', handlePointerMove);
+      canvas.removeEventListener('pointerdown', handlePointerDown);
+      canvas.removeEventListener('pointerup', handlePointerUp);
+      canvas.removeEventListener('pointermove', handlePointerMove);
       document.removeEventListener('keydown', handleKeyDown);
       document.removeEventListener('keyup', handleKeyUp);
     }
