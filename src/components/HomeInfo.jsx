dac467f13725c236f80f323d3e18fdeaf9e1da5c
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-20 22:53:14 +0100

    added button styling

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { arrow } from '../assets/icons';
 
-const HomeInfo = () => {
-  return (
-    <div>HomeInfo</div>
-  )
+const InfoBox = ({ text, link, btnText }) => (
+    <div className="info-box">
+        <p className="font-medium sm:text-xl text-center">{text}</p>
+        <Link to={link} className="neo-brutalism-white neo-btn">
+            {btnText}
+            <img src={arrow} />
+        </Link>
+    </div>
+)
+
+const renderContent = {
+    1: (
+        <h1 className="sm:text-xl sm:leading-snug text-center bg-gradient-to-r from-green-400 to-blue-500 py-4 px-8 text-white mx-5 rounded-full">
+            Hello, my name is <span className="font-semibold underline">Rene!</span> 😃
+            <br/>
+            An Aspiring Full-Stack Developer
+            </h1>
+    ),
+    2: (
+        <InfoBox text="Worked on many solo projects and picked up many knowledge along the way"
+        link="/about"
+        btnText="Learn more"
+        />
+    ),
+    3: (
+        <h1>3</h1>
+    ),
+    4: (
+        <h1>4</h1>
+    ),
+}
+
+const HomeInfo = ({ currentStage }) => {
+  return renderContent[currentStage] || null;
 }
 
 export default HomeInfo
