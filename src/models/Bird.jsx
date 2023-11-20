8e8ea5d6853051b840a24597a31cd520b1afdf52
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-20 14:21:10 +0100

    completed animations
    bird moving, plane moving, background moving, island moving - gud

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -2,6 +2,7 @@ import { useRef, useEffect } from 'react';
 
 import birdScene from '../assets/3d/bird.glb';
 import { useAnimations, useGLTF } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 
 const Bird = () => {
   const { scene, animations } = useGLTF(birdScene);
@@ -11,6 +12,29 @@ const Bird = () => {
   useEffect(() => {
     actions['Take 001'].play();
   }, []);
+
+  useFrame(({ clock, camera }) => {
+    // bird-like motion
+    birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2
+    // checking if bird reached certain point
+    if(birdRef.current.position.x > camera.position.x + 10) {
+      // change direction and rotate bird 180 degrees
+      birdRef.current.rotation.y = Math.PI;
+    } else if(birdRef.current.position.x < camera.position.x -10) {
+      // reset birds rotation and change direction to forward
+      birdRef.current.rotation.y = 0;
+    }
+    // update x and z axis based on direction
+    if(birdRef.current.rotation.y === 0) {
+      // moving forward
+      birdRef.current.position.x += 0.01;
+      birdRef.current.position.z -= 0.01;
+    } else {
+      // moving backwards
+      birdRef.current.position.x -= 0.01;
+      birdRef.current.position.z += 0.01;
+    }
+  });
     
   return (
     <mesh 
